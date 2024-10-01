266e00cec5f981b830eddd68e8d5697e7acba691
zhaiderabbas <zaki.hyder1994@gmail.com>
2024-10-01 12:12:09 +0500

    Assigment

---
+++ class2/src/app/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+export default function Home() {
+  return (
+    <>
+        <div className="container">
+          <div className="content">
+            <p className="intro-text">Hi, I am</p>
+            <h1 className="name">Zulqarnain haider</h1>
+            <h2 className="title">Junior Next JS Developer</h2>
+            <div className="icons">
+          
+            </div>
+          </div>
+       
+        <div className="image-container">
+          <img
+            src="/images/profile-image.png"
+            alt="Profile Picture"
+            className="profile-image"
+          />
+        </div>
+      </div>
+     
+    </>
+  );
+}
